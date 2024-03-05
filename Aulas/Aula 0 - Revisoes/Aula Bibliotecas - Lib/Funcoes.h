5093218d56cc004feb1540a615b0f1b81ddd652e
lufer <lufer@ipca.pt>
2024-03-05 10:12:19 +0000

    Revisões

---
+++ Aulas/Aula 0 - Revisoes/Aula Bibliotecas - Lib/Funcoes.h
@@ -0,0 +1,39 @@
+/*****************************************************************//**
+ * @file   Funcoes.h
+ * @brief  
+ * 
+ * @author lufer
+ * @date   February 2024
+ *********************************************************************/
+
+#pragma once
+
+//ou
+//#ifndef AAAAAA
+//#define AAAAAA BENFICA
+
+#define N 20
+
+typedef struct Pessoa {
+	int idade;
+	char nome[N];
+};
+
+
+typedef struct Calculo {
+	int soma;
+	int sub;
+	float div;
+	int mul;
+};
+
+/**
+ * Calcula a soma de dois valores inteiros
+ */
+int Soma(int x, int y);
+
+int Maior(int a, int b);
+
+//#endif // !AAAAAA
+
+
---
+++ Aulas/Aula 0 - Revisoes/Calculadora/Calculadora.c
@@ -0,0 +1,22 @@
+/*****************************************************************//**
+ * @file   Calculadora.c
+ * @brief  
+ * 
+ * @author lufer
+ * @date   February 2024
+ *********************************************************************/
+
+#include "Xiineses/Funcoes.h"
+#include <ctype.h>
+
+#pragma comment(lib,"Xiineses/Benfica.lib")
+
+int main() {
+
+	struct Calculo a;
+
+	int x = Soma(2, 3);
+
+	//getche();
+	return 0;
+}
